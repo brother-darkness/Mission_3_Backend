b0f5710eb006e60093f715bd2bedc46c9ea07f9d
Caleb Winters <calebw@missionreadyhq.com>
2022-08-31 21:48:42 +1200

    Endpoint added

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+app.post('/searchbar', (req, res) => {
+    let queryQuestion =req.body.searchbar
+
+    console.log(queryQuestion);
+    res.send('text')
+
 const DiscoveryV2 = require('ibm-watson/discovery/v2')
 const { IamAuthenticator } = require('ibm-watson/auth')
+
 
 const discovery = new DiscoveryV2 ({
     version:'2020-08-30',
@@ -18,9 +26,24 @@ const discovery = new DiscoveryV2 ({
         apikey: '0j3eG22iIMlRCXpT2tGpG3q2ztySie3oGw_0-Xu5830D',
       }),
       serviceUrl: 'https://api.au-syd.discovery.watson.cloud.ibm.com/instances/3e3b4eec-44e6-4267-ac7e-e4649464bfde',
+      disableSslVerification: true,
 
     });
+    // console.log('query point hit')
+    // discovery.query(queryParams)
+    // .then(queryResponse => {
+    //   console.log(JSON.stringify(queryResponse, null, 2));
+    //   res.send(JSON.stringify(queryResponse, null, 2));
+    // })
+    // .catch(err => {
+    //   console.log('error:', err);
+    // });
 
+});
+
+app.listen(4321, () => {
+    console.log('listening at port 4321')
+})
     
     
     

26653a9cb266d47d6cec68c2bcf044678a2280ed
Caleb Winters <calebw@missionreadyhq.com>
2022-09-02 15:50:09 +1200

    backend update

--- index.js
+++ index.js
@@ -10,36 +10,53 @@ app.use(cors())
 app.use(express.json())
 
 
-app.post('/searchbar', (req, res) => {
+app.get('/',(req,res) => {
+    const meesage ='Hitting the endpoint'
+
+    res.send(message)
+    console.log(message)
+})
+
+app.post('app/:query',(req,res) => {
+    const query = req.params.query
+
+    console.log(`Your question is "${query}"`)
+
+})
+
+
+app.post('/searchbar/:quesry', (req, res) => {
     let queryQuestion =req.body.searchbar
 
     console.log(queryQuestion);
-    res.send('text')
+    
 
-const DiscoveryV2 = require('ibm-watson/discovery/v2')
-const { IamAuthenticator } = require('ibm-watson/auth')
-
-
-const discovery = new DiscoveryV2 ({
-    version:'2020-08-30',
-    authenticator: new IamAuthenticator({
-        apikey: '0j3eG22iIMlRCXpT2tGpG3q2ztySie3oGw_0-Xu5830D',
+    const DiscoveryV2 = require('ibm-watson/discovery/v2');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+    
+    const discovery = new DiscoveryV2({
+      version: '2020-08-30',
+      authenticator: new IamAuthenticator({
+        apikey: 'GzeKliJ6_zLKwTkdfnO3VZ412daxyF6rT3Hs1veOitng',
       }),
-      serviceUrl: 'https://api.au-syd.discovery.watson.cloud.ibm.com/instances/3e3b4eec-44e6-4267-ac7e-e4649464bfde',
-      disableSslVerification: true,
+      serviceUrl: 'https://api.au-syd.discovery.watson.cloud.ibm.com/instances/3e3b4eec-44e6-4267-ac7e-e4649464bfde'
 
     });
-    // console.log('query point hit')
-    // discovery.query(queryParams)
-    // .then(queryResponse => {
-    //   console.log(JSON.stringify(queryResponse, null, 2));
-    //   res.send(JSON.stringify(queryResponse, null, 2));
-    // })
-    // .catch(err => {
-    //   console.log('error:', err);
-    // });
-
-});
+    
+    const params = {
+      projectId: '392ced84-5c05-4957-bc41-b1218421f536',
+      query: 'text:car',
+    };
+    
+    discovery.query(params)
+      .then(response => {
+        console.log(JSON.stringify(response.result, null, 2));
+        res.send(JSON.stringify(response.result, null, 2));
+      })
+      .catch(err => {
+        console.log('error:', err);
+      });
+    });
 
 app.listen(4321, () => {
     console.log('listening at port 4321')
